23bf9048194c7dc09b827ffe90967b6208935895
Raul P. Murguia <murguia@users.noreply.github.com>
2022-01-01 11:34:43 -0600

    next changes

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,44 @@ export type ExProp = { name: string; val: number };
 export type PrevExMap = { [name: string]: ExProp[] };
 
 const prevDates: string[] = ["12/21/2020", "12/20/2020", "12/19/2020"];
-const realEx: ExProp[] = [
+const currEx: ExProp[] = [
   { name: "ex0", val: 0 },
   { name: "ex1", val: 1 },
   { name: "ex2", val: 2 }
 ];
+
+const pastExNames: ExProp[] = [
+  { name: "ex3", val: 3 },
+  { name: "ex4", val: 4 }
+];
+
+const pastExData: PrevExMap = {
+  ex0: [
+    { name: "ex0", val: 0 },
+    { name: "ex0", val: 0 },
+    { name: "ex0", val: 0 }
+  ],
+  ex1: [
+    { name: "ex1", val: 1 },
+    { name: "ex1", val: 1 },
+    { name: "ex1", val: 1 }
+  ],
+  ex2: [
+    { name: "ex2", val: 2 },
+    { name: "ex2", val: 2 },
+    { name: "ex2", val: 2 }
+  ],
+  ex3: [
+    { name: "ex3", val: 3 },
+    { name: "ex3", val: 3 },
+    { name: "ex3", val: 3 }
+  ],
+  ex4: [
+    { name: "ex4", val: 4 },
+    { name: "ex4", val: 4 },
+    { name: "ex4", val: 4 }
+  ]
+};
 
 const headerData: string[] = ["col 1", "col 2", "col 3"];
 const bodyData: string[][] = [
